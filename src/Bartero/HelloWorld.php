72cb875741a1fd9e3862b24d841b2308bc1fa0f5
Bartłomiej Krzywania <bartek.krzywania@gmail.com>
2017-11-22 00:08:55 +0100

    Changed sth to test the integration with Packagist

--- src/Bartero/HelloWorld.php
+++ src/Bartero/HelloWorld.php
@@ -5,11 +5,11 @@ use Bartero\Config;
 
 class HelloWorld {
    private $logger;
-   public HelloWorld() {
+   public function __construct() {
       $this->logger = Config::getLogger();
    }
 
    public function sayHello() {
-      $this->logger->info("Hello Guys!");
+      $this->logger->info("Hello Guys for the 0.2 time!");
    }
 }
